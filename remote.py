7e01e948315f118e4c246b9fd0bda0992e498acf
alberto-abarzua <uintuser@pm.me>
2023-10-28 19:25:50 -0300

    feat: initial commit

---
+++ remote.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import argparse
+import subprocess
+import os
+
+CURRENT_DIR = Path(__file__).parent.absolute()
+
+
+def load_env():
+    with open(CURRENT_DIR / ".env") as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            key, value = line.strip().split("=")
+            os.environ[key] = value
+
+
+def build_image(folder_name, docker_repo):
+    dockerfile_dir = str(CURRENT_DIR / "dockerfiles" / folder_name)
+    image_tag = f"{docker_repo}:{folder_name}"
+    subprocess.run(["docker", "build", "-t", image_tag, dockerfile_dir])
+
+
+def push_image(folder_name, docker_repo):
+    image_tag = f"{docker_repo}:{folder_name}"
+    subprocess.run(["docker", "push", image_tag])
+
+
+def build_all(docker_repo):
+    dockerfiles_dir = CURRENT_DIR / "dockerfiles"
+    for folder in os.listdir(dockerfiles_dir):
+        if os.path.isdir(dockerfiles_dir / folder):
+            build_image(folder, docker_repo)
+
+
+def push_all(docker_repo):
+    dockerfiles_dir = CURRENT_DIR / "dockerfiles"
+    for folder in os.listdir(dockerfiles_dir):
+        if os.path.isdir(dockerfiles_dir / folder):
+            push_image(folder, docker_repo)
+
+
+def main():
+    load_env()
+    parser = argparse.ArgumentParser(description="Docker image builder and pusher")
+    subparsers = parser.add_subparsers(dest="action")
+
+    build_parser = subparsers.add_parser("build")
+    build_parser.add_argument("folder", type=str, help="Name of the folder containing the Dockerfile")
+
+    push_parser = subparsers.add_parser("push")
+    push_parser.add_argument("folder", type=str, help="Name of the folder containing the Dockerfile")
+
+    build_all_parser = subparsers.add_parser("build-all")
+
+    push_all_parser = subparsers.add_parser("push-all")
+
+    args = parser.parse_args()
+
+    docker_repo = os.environ["DOCKER_REPO"]
+
+    if args.action == "build":
+        build_image(args.folder, docker_repo)
+    elif args.action == "push":
+        push_image(args.folder, docker_repo)
+    elif args.action == "build-all":
+        build_all(docker_repo)
+    elif args.action == "push-all":
+        push_all(docker_repo)
+
+
+if __name__ == "__main__":
+    main()
